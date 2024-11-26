d8ad7a3bbf74bda5035fb1224bfe2a3648169bb4
Gustavo Wippel <gustavo.wippel@paytrack.com.br>
2024-11-26 13:59:21 -0300

    feat(PTF): adicionando commit liint 123183

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from "@commitlint/types";
 const Configuration: UserConfig = {
   parserPreset: {
     parserOpts: {
-      headerPattern: /^(\w+)\((FIN|PTF|PAY)-(\d+)\): (.+)$/,
+      headerPattern: /^(\w+)\((FIN|PTF|PAY)\): (.+)$/,
       headerCorrespondence: ["type", "scope", "id", "subject"],
     },
   },
@@ -13,19 +13,18 @@ const Configuration: UserConfig = {
       "always",
       ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
     ],
-    "subject-empty": [2, "never"],
+    ["custom-scope"]: [2, "always"],
   },
   plugins: [
     {
       rules: {
         // Validação personalizada para garantir que o scope esteja correto
-        "custom-scope": ({ scope, subject }) => {
+        "custom-scope": ({ id, scope }) => {
           // Regex para garantir que o scope tenha o formato adequado
-          const validScopePattern = /^(FIN|PTF|PAY)-\d+$/;
+          const validScopePattern = /^(FIN|PTF|PAY)$/;
 
-          console.log("scope", scope);
-          console.log("subject", subject);
-          if (!scope || !validScopePattern.test(scope)) {
+          console.log(id, scope);
+          if (!scope || !validScopePattern.test(scope) || id) {
             return [
               false,
               "O escopo do commit deve ser um dos prefixos FIN, PTF ou PAY seguido de um número, ex: PTF-123.",
