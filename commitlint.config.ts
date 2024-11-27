4f6d3013188a08783469fb9cf58142238c826977
Gustavo Wippel <gustavo.wippel@paytrack.com.br>
2024-11-27 01:32:43 -0300

    feat(PTF-1): opa estou criando coisas boas

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,44 +1,191 @@
-import type { UserConfig } from "@commitlint/types";
+// import type { UserConfig } from "@commitlint/types";
 
-const Configuration: UserConfig = {
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w+)\((FIN|PTF|PAY)\): (.+)$/,
-      headerCorrespondence: ["type", "scope", "id", "subject"],
-    },
-  },
+// const Configuration: UserConfig = {
+//   extends: ["@commitlint/config-conventional"],
+//   parserPreset: {
+//     parserOpts: {
+//       headerPattern: /^(\w+)\((FIN|PTF|PAY)\): (.+)$/,
+//       headerCorrespondence: ["type", "scope", "id", "subject"],
+//     },
+//   },
+//   rules: {
+//     "type-enum": [
+//       2,
+//       "always",
+//       ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+//     ],
+//     ["task"]: [2, "always"],
+//   },
+//   plugins: [
+//     {
+//       rules: {
+//         task: ({ id, scope, subject }) => {
+//           // Regex para garantir que o scope tenha o formato adequado
+//           const validScopePattern = /^(FIN|PTF|PAY)$/;
+
+//           //TODO: Ajustar para ver a questão de adicionar numero da task
+//           console.log(subject);
+//           console.log("id", id);
+//           console.log("scope", scope);
+//           if (!scope || !validScopePattern.test(scope)) {
+//             return [
+//               false,
+//               "O escopo do commit deve ser um dos prefixos FIN, PTF ou PAY seguido de um número, ex: PTF-123.",
+//             ];
+//           }
+
+//           return [true]; // Se o escopo for válido
+//         },
+//       },
+//     },
+//   ],
+// };
+
+// export default Configuration;
+
+import {
+  RuleConfigCondition,
+  RuleConfigSeverity,
+  TargetCaseType,
+} from "@commitlint/types";
+
+export default {
+  parserPreset: "conventional-changelog-conventionalcommits",
   rules: {
-    "type-enum": [
-      2,
+    "body-leading-blank": [RuleConfigSeverity.Warning, "always"] as const,
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 100] as const,
+    "footer-leading-blank": [RuleConfigSeverity.Warning, "always"] as const,
+    "footer-max-line-length": [
+      RuleConfigSeverity.Error,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
-    ],
-    ["custom-scope"]: [2, "always"],
+      100,
+    ] as const,
+    "header-max-length": [RuleConfigSeverity.Error, "always", 100] as const,
+    "header-trim": [RuleConfigSeverity.Error, "always"] as const,
+    "subject-case": [
+      RuleConfigSeverity.Error,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ] as [RuleConfigSeverity, RuleConfigCondition, TargetCaseType[]],
+    "subject-empty": [RuleConfigSeverity.Error, "never"] as const,
+    "subject-full-stop": [RuleConfigSeverity.Error, "never", "."] as const,
+    "type-case": [RuleConfigSeverity.Error, "always", "lower-case"] as const,
+    "type-empty": [RuleConfigSeverity.Error, "never"] as const,
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ] as [RuleConfigSeverity, RuleConfigCondition, string[]],
   },
-  plugins: [
-    {
-      rules: {
-        // Validação personalizada para garantir que o scope esteja correto
-        "custom-scope": ({ id, scope }) => {
-          // Regex para garantir que o scope tenha o formato adequado
-          const validScopePattern = /^(FIN|PTF|PAY)$/;
-
-          //TODO: Ajustar para ver a questão de adicionar numero da task
-          console.log(id, scope);
-          console.log("id", id);
-          console.log("scope", scope);
-          if (!scope || !validScopePattern.test(scope)) {
-            return [
-              false,
-              "O escopo do commit deve ser um dos prefixos FIN, PTF ou PAY seguido de um número, ex: PTF-123.",
-            ];
-          }
-
-          return [true]; // Se o escopo for válido
+  prompt: {
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: "A new feature",
+            title: "Features",
+            emoji: "✨",
+          },
+          fix: {
+            description: "A bug fix",
+            title: "Bug Fixes",
+            emoji: "🐛",
+          },
+          docs: {
+            description: "Documentation only changes",
+            title: "Documentation",
+            emoji: "📚",
+          },
+          style: {
+            description:
+              "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+            title: "Styles",
+            emoji: "💎",
+          },
+          refactor: {
+            description:
+              "A code change that neither fixes a bug nor adds a feature",
+            title: "Code Refactoring",
+            emoji: "📦",
+          },
+          perf: {
+            description: "A code change that improves performance",
+            title: "Performance Improvements",
+            emoji: "🚀",
+          },
+          test: {
+            description: "Adding missing tests or correcting existing tests",
+            title: "Tests",
+            emoji: "🚨",
+          },
+          build: {
+            description:
+              "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+            title: "Builds",
+            emoji: "🛠",
+          },
+          ci: {
+            description:
+              "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+            title: "Continuous Integrations",
+            emoji: "⚙️",
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: "Chores",
+            emoji: "♻️",
+          },
+          revert: {
+            description: "Reverts a previous commit",
+            title: "Reverts",
+            emoji: "🗑",
+          },
         },
       },
+      scope: {
+        description:
+          "What is the scope of this change (e.g. component or file name)",
+      },
+      subject: {
+        description:
+          "Write a short, imperative tense description of the change",
+      },
+      body: {
+        description: "Provide a longer description of the change",
+      },
+      isBreaking: {
+        description: "Are there any breaking changes?",
+      },
+      breakingBody: {
+        description:
+          "A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself",
+      },
+      breaking: {
+        description: "Describe the breaking changes",
+      },
+      isIssueAffected: {
+        description: "Does this change affect any open issues?",
+      },
+      issuesBody: {
+        description:
+          "If issues are closed, the commit requires a body. Please enter a longer description of the commit itself",
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      },
     },
-  ],
+  },
 };
-
-export default Configuration;
